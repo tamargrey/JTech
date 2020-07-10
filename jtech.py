345861ebe3e19f7d77dc2760d05be458bd485a70
Joshua Fishman <joshuafishman1@gmail.com>
2020-07-10 00:43:16 -0400

    parse out literals

--- jtech.py
+++ jtech.py
@@ -14,7 +14,7 @@ def format_week(filename):
                     continue
                 if key == "Timestamp":
                     continue
-                if len(entry) > 1 and entry[1] == ')':
+                if len(entry) > 1 and entry[1] == ')':  # multiple choice
                     continue
                 if key == "Name, please!":
                     person_responses.append("\\headline{%s} \n" % entry)
@@ -25,9 +25,16 @@ def format_week(filename):
                     key = "And Now: How was your week?  (bonus points if your word count is a palindrome or multiple of 11)"
                     word_count = len(entry.split())
                     entry = "Word Count: {}\n\n".format(word_count) + entry
+
+                for literal in '&#':
+                    if literal in entry:
+                        loc = entry.find(literal)
+                        entry = entry[:loc] + '\\' + entry[loc:]
+
                 person_responses.append("\\noindent \\textbf{{{}}}:\n\n {}\n\n".format(key, entry))
             summaries.append("\n".join(person_responses))
 
+    summaries.append("") # final \closearticle
     return "\\closearticle".join(summaries)
             # summary = row["summary"]
             # summary_len = len(summary.split(' '))
