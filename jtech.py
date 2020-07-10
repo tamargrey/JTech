1273b45ad6b40885fc6968658a53632824a399dd
Tamar Grey <tamargrey@Tamars-MBP.attlocal.net>
2020-07-10 14:31:05 -0500

    Add ability to plot pie chart multiplt choice

--- jtech.py
+++ jtech.py
@@ -1,99 +1,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-def splitword(sentence, max_line):
-    count = 0
-    for word in sentence.split():
-        count += len(word) + 1
-        if count > max_line:
-            return sentence[:count] + "\n" + sentence[count:]
-    # print(count)
-    return sentence
-
-
-def format_week(filename):
-    responses = []
-    multiple_choice = {}
-
-    with open('{}.csv'.format(filename), newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            person_responses = []
-            name_key = "Name, please!"
-            name = row.pop(name_key)
-            person_responses = ["\\headline{%s} \n"%name]
-
-            for key, entry in row.items():
-                for literal in '&#{}':
-                    if literal in entry:
-                        loc = entry.find(literal)
-                        entry = entry[:loc] + '\\' + entry[loc:]
-                    if literal in key:
-                        loc = key.find(literal)
-                        key = key[:loc] + '\\' + key[loc:]
-
-                for weird_character in "â€™":
-                    if weird_character in entry:
-                        loc = entry.find(weird_character)
-                        entry = entry[:loc] + entry[loc + 1:]
-                    if weird_character in key:
-                        loc = key.find(weird_character)
-                        key = key[:loc] + key[loc + 1:]
-
-                entry = entry.replace('\n', '\n\n').replace('\n\n\n', '\n\n')
-
-                if not entry:
-                    continue
-
-                if key == "Timestamp":
-                    continue
-
-                if len(entry) > 1 and entry[1] == ')':  # multiple choice
-                    question = multiple_choice.setdefault(key, {})
-                    question[entry] = question.get(entry, 0) + 1
-                    continue
-
-                if key == "Any pictures you'd like to share?":
-                    entry = "%\\includegraphics[width=1.5in]{{{}}}".format(name)  # will need to uncomment this and add the picture name in latex
-
-                if key == "And Now: How was your week?  (bonus points if your word count is a palindrome or multiple of 11) (gentle reminder for third person responses, please!)":
-                    key = "And Now: How was your week?  (bonus points if your word count is a palindrome or multiple of 11)"
-                    word_count = len(entry.split())
-                    entry = "Word Count: {}\n\n".format(word_count) + entry
-
-                person_responses.append("\n \\noindent \\textbf{{{}}}:\n\n {}\n".format(key, entry))
-
-            responses.append("\n".join(person_responses))
-
-    responses.append("\n \\columnbreak \n")
-
-    for i, (question, answers) in enumerate(multiple_choice.items()):
-        fname = "MultipleChoice{}.jpg".format(i)
-        choices, nums = answers.keys(), answers.values()
-        choices = [splitword(choice, 50) for choice in choices]
-
-        fig1, ax1 = plt.subplots()
-        wedges, _, _ = ax1.pie(list(nums), autopct='%1.0f%%', shadow=True, startangle=90)
-        ax1.legend(wedges, choices)
-        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-        plt.savefig(fname)
-
-        multiple_responses = []
-        multiple_responses.append("\n\n \\noindent \\textbf{{{}}}:\n".format(question))
-        multiple_responses.append("\\includegraphics[width=\columnwidth]{{{}}} \n".format(fname))
-        responses.append("".join(multiple_responses))
-
-    responses.append("")  # final \closearticle
-    body = "\\closearticle".join(responses)
-    f = open("body.tex", "a")
-    f.write(body)
-    f.close()
-
-# print(format_week("JTech Week 17 - The 3 Weeks  (Responses) - Form Responses 1"))
-
+from collections import Counter
 
 
 '''
@@ -161,15 +69,18 @@ def get_weeks(filename, name_col, summary_col):
 
 '''
 Gets everyone's answers for a specific question
-NOTE - ignores unansswered questions
+NOTE - ignores unansswered questions and will work well with short answer-style qs
 
 Inputs:
     filename - csv file name string with responses - the file must be in the same directory 
     headline - string, your desired header
     question_col - column name of the question you'd like to format
     name_col -string of the column/question asking for peoples' names- must be exact 
+
+Output:
+    latex file called <question_col>.tex - this might be kind of long and unweidly
 '''
-def get_question(filename, headline, question_col, name_col):
+def get_short_answer_question_with_names(filename, headline, question_col, name_col):
     with open('{}.csv'.format(filename), newline='') as csvfile:
         answers = []
         reader = csv.DictReader(csvfile)
@@ -186,3 +97,48 @@ def get_question(filename, headline, question_col, name_col):
     f.close() 
 
 # get_question('summary','Hours', 'How many hours did you sleep last night? ', 'Name')
+
+def splitword(sentence, max_line):
+    count = 0
+    for word in sentence.split():
+        count += len(word) + 1
+        if count > max_line:
+            return sentence[:count] + "\n" + sentence[count:]
+    return sentence
+
+
+'''
+Allows you to plot pie chart with legend
+
+Inputs:
+    filename - csv file name string with responses - the file must be in the same directory 
+    headline - string, your desired header
+    question_col - column name of the question you'd like to format
+Output:
+    <question_col>.jpg
+'''
+def get_multiple_choice_question_pie(filename, headline, question_col):
+    answers = []
+    with open('{}.csv'.format(filename), newline='') as csvfile:
+        answers = []
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+           
+           answers.append(row[question_col])
+        #    want to grab answer
+    answers_hist = Counter(answers)
+    
+    choices = [splitword(choice, 50) for choice in list(answers_hist.keys())]
+    # build plot
+    fname = question_col + ".jpg"
+    fig1, ax1 = plt.subplots()
+    wedges, _, _ = ax1.pie(list(answers_hist.values()), autopct='%1.0f%%', shadow=True, startangle=90)
+    ax1.legend(wedges, list(choices), loc = 'lower right') #the legend is over the pie
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.savefig(fname)
+
+get_multiple_choice_question_pie('summary', "Q", 'How did MIT *actually* decide who got to return?')
+
+
+
+
