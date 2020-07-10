13f676178551cecef6ab0d8448fbfd9172c72c46
Tamar Grey <tamargrey@Tamars-MBP.attlocal.net>
2020-07-09 21:53:49 -0500

    add sample csv

--- jtech.py
+++ jtech.py
@@ -1,20 +1,19 @@
-# import csv with jtech data 
 import csv
 
 
 def format_week(jtech_data):
     summaries = []
 
-    # Note: need to have renames columns to summary, one, and name
+    # Note: need to have renamed columns to summary, one, and name
     with open('jtech.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            # do thing to pull out info 
             summary = row["summary"]
             summary_len = len(summary.split(' '))
             one_word = row["one"]
             name = row["name"]
 
+            # couldnt remember how to put strings on separate lines
             summaries.append("\\headline{%s} \n \n  One word summary: %s \n \n Word Count: %d \n \n %s \n \\closearticle" % (name, one_word, summary_len, summary))
     return summaries
 print ("".join(format_week("")))
